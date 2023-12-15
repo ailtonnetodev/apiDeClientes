278d29ce4ffeda9094c4eab620d86d50f98ee782
ailtonnetodev <ailtonnetto8319@gmail.com>
2023-12-15 15:49:17 -0300

    Cadastrando novo cliente

---
+++ src/controllers/CreateCustomerController.ts
@@ -0,0 +1,16 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerService } from "../services/createCustomerService";
+
+class CreateCustomerController {
+    async handle(request: FastifyRequest, replay: FastifyReply) {
+
+        const { name, email } = request.body as { name: string, email: string};
+
+        const customerService = new CreateCustomerService()
+        const customer = await customerService.execute({ name, email });
+
+        replay.send(customer)
+    }
+}
+
+export { CreateCustomerController }
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply} from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyReply) { 
     fastify.get("/teste", async (request: FastifyRequest, replay: FastifyReply) => {
         return { message: "Teste de rota" }
+    })
+
+    fastify.post("/customer", async (request: FastifyRequest, replay: FastifyReply) => {
+        return new CreateCustomerController().handle(request, replay)
     })
 }
